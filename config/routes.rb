424ef64c1789de5d11501ed8574455adefc21524
MauricioNC <mauricionc19@gmail.com>
2023-10-19 13:00:37 -0600

    feat: Products views
    The /products/new view renders the form to create a new product.
    The form, contains a field_for "products[]", by this way, is possible to
    insert multiple products in the same request, then the controller catch
    this array and permit all the parameters.
    
    Also was created the resources for products.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :customers, only: [:new, :create, :show, :update]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :products, only: [:index, :new, :create, :show]
 
   # Defines the root path route ("/")
   root "customers#new"
+
 end
