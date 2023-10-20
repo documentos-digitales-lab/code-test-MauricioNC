6d4c95ea063d15a4bbcbf4c8937112260559c437
MauricioNC <mauricionc19@gmail.com>
2023-10-20 01:26:54 -0600

    feat: InvoiceProducts model create. This table has a many to many association between products and invoice

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,3 +1,6 @@
 class Invoice < ApplicationRecord
   belongs_to :customer
+  has_many :invoice_products
+  has_many :products, through: :invoice_products
+  validates :sub_total, :tax, :total, presence: true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :customer
+  has_many :invoice_products
+  has_many :invoices, through: :invoice_products
   validates :quantity, :product, :unit_price, :amount, presence: true
 end
